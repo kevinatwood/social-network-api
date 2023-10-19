62fbf2cabb73a72a21c0438339fb072126ad86bb
kevinatwood <atwoodkevin22@gmail.com>
2023-10-19 15:37:13 -0500

    funished delete route in userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -58,10 +58,9 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const id = req.params.id
-        const user = User.findByIdAndDelete(id)
+        const user = await User.findByIdAndDelete(req.params.id)
     console.log(user)
-    res.status(200).json({ message: 'User deleted successfully' });
+    res.status(200).json(user);
 }catch (err) {
     res.status(500).json(err)
     console.error(err)
