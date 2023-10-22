dd532aa2d39f4d702353346890a45a0cc39584b1
kevinatwood <atwoodkevin22@gmail.com>
2023-10-22 12:40:41 -0500

    created post and delete routes for the reactions. Need debugging

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -81,6 +81,50 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.post('/:thoughtId/reactions', async (req, res) => {
-    
+    try {
+        const thought = await Thought.findOne({_id: req.params.thoughtId})
+    if (!thought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+      const reaction = {
+        reactionBody: req.body.reactionBody,
+        username: req.body.username
+    }
+     
+     thought.reactions.push(reaction);
+     console.log(thought)
+     await thought.save()
+    res.status(200).json(thought)
+} catch(err){
+    res.status(500).json(err)
+    console.error(err)
+}
+});
+
+router.delete('/:thoughtId/reactions', async (req, res) => {
+    try{
+        const thought = await Thought.findOne({_id: req.params.thoughtId})
+    if (!thought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+      
+      const thoughtReactions = thought.reactions || [];
+      const reactions = thoughtReactions.map(i => i.reactionId);
+      console.log(reactions[2], req.body.reactionId, `new ObjectId("${req.body.reactionId}")`, reactions[2] == `new ObjectId("${req.body.reactionId}")`)
+
+      if (reactions.includes(req.body.reactionId)){ 
+        
+        thoughtReactions.forEach((el, index) => {
+        if (el.reactionId == req.body.reactionId){
+            thoughtReactions.splice(index, 1)
+        }
+      });
+    }
+    // await Thought.save()
+    res.status(200).json(thought)
+    }catch(err){
+        res.status(500).json(err)
+        console.error(err)
+    }
 })
 module.exports = router
